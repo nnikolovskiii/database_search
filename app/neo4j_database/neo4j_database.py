cab984f7c3aabbc94efa52106f839f8928cb3955
Nikola <nikolovski.nikola42@gmail.com>
2024-08-01 20:17:59 +0200

    Addded functionallity for neo4j.

--- app/neo4j_database/neo4j_database.py
+++ app/neo4j_database/neo4j_database.py
@@ -1,4 +1,7 @@
+from typing import Any, Dict
+
 from neo4j import GraphDatabase
+from pydantic import BaseModel
 
 uri = "bolt://localhost:7687"
 username = "neo4j"
@@ -6,15 +9,52 @@ password = "Test09875"
 
 driver = GraphDatabase.driver(uri, auth=(username, password))
 
-def run_query(driver, query):
+
+class Node(BaseModel):
+    type: str
+    properties: Dict[str, Any]
+
+
+class Relationship(BaseModel):
+    type: str
+
+
+def _transform_properties(properties: Dict[str, Any]) -> str:
+    return ", ".join(f"{key}: '{value}'" for key, value in properties.items())
+
+
+def create_node(node: Node):
     with driver.session() as session:
-        result = session.run(query)
-        return list(result) 
+        properties = _transform_properties(node.properties)
+        cypher_query = f"CREATE (n:{node.type} {{{properties}}}) RETURN n"
+        session.run(cypher_query)
 
-query = "MATCH (n) RETURN n"
 
-result = run_query(driver, query)
-for record in result:
-    print(record)
+def node_exists(node: Node) -> bool:
+    with driver.session() as session:
+        label = node.type
+        label_existence_query = f"CALL db.labels() YIELD label RETURN label"
+        labels_result = session.run(label_existence_query)
+        labels = [record["label"] for record in labels_result]
 
-driver.close()
+        if label not in labels:
+            return False
+
+        properties = _transform_properties(node.properties)
+        cypher_query = f"MATCH (n:{label} {{{properties}}}) RETURN n"
+        result = session.run(cypher_query)
+        return result.single() is not None
+
+
+def create_relationship(node1: Node, node2: Node, relationship: Relationship):
+    if not node_exists(node1):
+        create_node(node1)
+    if not node_exists(node2):
+        create_node(node2)
+
+    with driver.session() as session:
+        properties1 = _transform_properties(node1.properties)
+        properties2 = _transform_properties(node2.properties)
+        cypher_query = f"""MATCH (node1:{node1.type} {{{properties1}}}), (node2:{node2.type} {{{properties2}}})
+        CREATE (node1)-[:{relationship.type}]->(node2)"""
+        session.run(cypher_query)
