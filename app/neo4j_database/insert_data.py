b59e3b50337a2849e4753bba9b122fc60ca4efaf
Nikola <nikolovski.nikola42@gmail.com>
2024-08-02 15:05:25 +0200

    Added table nodes and connections in neo4j graph database.

--- app/neo4j_database/insert_data.py
+++ app/neo4j_database/insert_data.py
@@ -1,2 +1,42 @@
-while True:
-    print("lol")
+from typing import List, Tuple
+
+from app.neo4j_database.neo4j_database import create_relationship, Node, Relationship
+from app.sql_database.database_connection import get_tables_with_foreign_keys
+from pydantic import BaseModel
+
+
+class ForeignKey(BaseModel):
+    from_column: str
+    to_column: str
+    to_table: str
+
+    def __init__(self, data: Tuple[str]):
+        super().__init__(
+            from_column=data[0],
+            to_column=data[1],
+            to_table=data[2]
+        )
+
+
+def insert_tables_with_foreign_keys(
+
+):
+    tables = get_tables_with_foreign_keys()
+    for table_name, foreign_keys in tables.items():
+        fks = [ForeignKey(foreign_key) for foreign_key in foreign_keys]
+
+        for fk in fks:
+            create_relationship(
+                node1=Node(
+                    type="Table",
+                    properties={"name": fk.to_table}
+                ),
+                node2=Node(
+                    type="Table",
+                    properties={"name": table_name}
+                ),
+                relationship=Relationship(type="FOREIGN_KEY")
+            )
+
+
+insert_tables_with_foreign_keys()
--- app/sql_database/database_connection.py
+++ app/sql_database/database_connection.py
@@ -1,17 +1,59 @@
-from typing import List, Tuple
-
+from typing import List, Tuple, Dict, Any
 import psycopg2
 
 from app.models.enums.postgres_data_types import PostgresDataType
-from app.vectorstore.qdrant import upsert_record
 
 db_config = {
-    'dbname': 'sample-sql_database',
+    'dbname': 'sample-database',
     'user': 'postgres',
     'password': 'postgres',
     'host': 'localhost',
     'port': '9871'
 }
+
+
+def get_tables_with_foreign_keys(
+        table_schema: str = "public"
+) -> Dict[str, Any]:
+    conn = psycopg2.connect(**db_config)
+    cur = conn.cursor()
+
+    cur.execute("""
+        SELECT table_name
+        FROM information_schema.tables
+        WHERE table_schema = %s;
+    """, (table_schema,))
+    tables = cur.fetchall()
+    tables = [table[0] for table in tables]
+
+    all_foreign_keys = {}
+
+    for table in tables:
+        cur.execute("""
+            SELECT
+                tc.constraint_name, 
+                kcu.column_name, 
+                ccu.table_name AS foreign_table_name
+            FROM 
+                information_schema.table_constraints AS tc 
+                JOIN information_schema.key_column_usage AS kcu
+                  ON tc.constraint_name = kcu.constraint_name
+                JOIN information_schema.constraint_column_usage AS ccu 
+                  ON ccu.constraint_name = tc.constraint_name
+            WHERE tc.constraint_type = 'FOREIGN KEY' 
+              AND tc.table_name = %s
+              AND tc.table_schema = %s;
+        """, (table, table_schema))
+
+        foreign_keys = cur.fetchall()
+
+        if foreign_keys:
+            all_foreign_keys[table] = foreign_keys
+
+    cur.close()
+    conn.close()
+
+    return all_foreign_keys
 
 
 def get_tables(
